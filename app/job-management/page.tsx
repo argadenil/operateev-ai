2847c983334b7661f8b3e1682cdd52c6e496ed61
nilesh <nilesha@perscitussln.com>
2025-08-30 19:23:26 +0530

    removed overflow

--- app/job-management/page.tsx
+++ app/job-management/page.tsx
@@ -304,7 +304,7 @@ export default function JobManagementPage() {
   if (loading) return <Loader />;
 
   return (
-    <div className="p-8 h-[85vh] overflow-y-auto">
+    <div className="p-8 h-[85vh]">
       <div className="max-w-7xl mx-auto flex flex-col gap-8">
         {/* Page Header */}
 
