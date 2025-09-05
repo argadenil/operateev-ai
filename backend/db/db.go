d15f7d2cb4af1cb5923ec29f2f78f5f7eaec9a13
nilesh <nilesha@perscitussln.com>
2025-09-05 16:54:27 +0530

    updates db url

--- backend/db/db.go
+++ backend/db/db.go
@@ -28,7 +28,7 @@ func Connect(dsn string) error {
 
 // InitializeDB connects to the database using the default DSN and sets up the global connection.
 func InitializeDB() error {
-	dsn := "postgresql://operateev_ai_user:Ivl7TSwqxRCKXQ7aq0Lwm2rFpGHG6YhD@dpg-d2taiube5dus73dldfo0-a/operateev_ai"
+	dsn := "postgresql://operateev_ai_user:Ivl7TSwqxRCKXQ7aq0Lwm2rFpGHG6YhD@dpg-d2taiube5dus73dldfo0-a.ohio-postgres.render.com/operateev_ai"
 	if err := Connect(dsn); err != nil {
 		log.Fatal("db connection failed: ", err)
 	}
