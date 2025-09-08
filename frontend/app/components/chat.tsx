3a73f88530f6fd8e21ef74ca5703a4e7f7e61d6b
nilesh <nilesha@perscitussln.com>
2025-09-08 16:21:01 +0530

    added 8000 port

--- frontend/app/components/chat.tsx
+++ frontend/app/components/chat.tsx
@@ -35,7 +35,7 @@ export default function Chat() {
     setIsProcessing(true);
 
     try {
-      const res = await fetch("http://localhost:10000/chat", {
+      const res = await fetch("http://localhost:8000/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
